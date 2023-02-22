88871e0be10375757205ad18d323f7261365221e
Taro <taronull@gmail.com>
2023-02-22 09:54:56 -0500

    Revise deprecated form convention

--- lib/feder/auth/access.live.ex
+++ lib/feder/auth/access.live.ex
@@ -5,7 +5,7 @@ defmodule Feder.Auth.Access.Live do
 
   def render(assigns) do
     ~H"""
-    <.form for={:account} phx-submit="mail_access" class="space-y-4">
+    <.form for={%{}} as={:account} phx-submit="mail_access" class="space-y-4">
       <.heading>Continue with Email</.heading>
 
       <.input name="email" type="email" placeholder="Your email" />
